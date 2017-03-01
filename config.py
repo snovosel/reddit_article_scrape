e0e4ba0d62eae3c267e05512650e200e6881fcc9
Steve Novosel <novosel991@gmail.com>
2017-03-01 09:21:46 -0500

    updated config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class BaseConfiguration(object):
     MAIL_SERVER = 'smtp.gmail.com'
@@ -7,3 +12,8 @@ class BaseConfiguration(object):
     MAIL_USERNAME = ''
     MAIL_PASSWORD = ''
     MAIL_DEFAULT_SENDER = ''
+
+
+class Config(object):
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = ''
