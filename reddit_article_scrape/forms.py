bff593aed977a4195aaed4f0ffb9980efee0ef2b
Steve Novosel <novosel991@gmail.com>
2017-03-26 17:31:50 -0400

    complete bootstrap styling

--- reddit_article_scrape/forms.py
+++ reddit_article_scrape/forms.py
@@ -3,13 +3,13 @@ from wtforms import StringField, validators, Form, PasswordField
 from wtforms.validators import DataRequired
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    username = StringField('Username', [validators.Length(min=4, max=25)], render_kw={"placeholder": "Username"})
+    email = StringField('Email Address', [validators.Length(min=6, max=35)], render_kw={"placeholder": "Email"})
     password = PasswordField('New Password', [ validators.DataRequired(), validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
+    ], render_kw={"placeholder": "Password"})
+    confirm = PasswordField('Repeat Password', render_kw={"placeholder": "Confirm Password"})
 
 
 class LoginForm(Form):
-    username = StringField('Username')
-    password = PasswordField('Password')
+    username = StringField('Username', render_kw={"placeholder": "Username"})
+    password = PasswordField('Password', render_kw={"placeholder": "Password"})
